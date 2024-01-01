6486e96f7e1956d77b80c6accb12339d75a4e91e
Derek Smith <dkbsmith03@gmail.com>
2024-01-01 13:17:30 -0600

    MAB_datagen_finished

--- Data_Gen.py
+++ Data_Gen.py
@@ -4,15 +4,43 @@ import torch.nn.functional as F
 import numpy 
 import random
 import math 
+import matplotlib.pyplot as plt
+import datetime
+from torch.utils.tensorboard import SummaryWriter
 
-A = 5
-T = 20
+writer = SummaryWriter()
+
+N = 5
+T = 200
 
 
 class Arm():
-    def __init__(self, p, T):
-       self.prob = p
-       self.steps = T
-    def gen(self):
-        for i in range(self.steps):
-            
+    def __init__(self):
+        pass
+    def gen(self,mu, sig):
+        spun = numpy.random.normal(mu, sig, T)
+        return spun
+
+class Arms():
+    def __init__(self, N):
+        self.Num = N
+        self.arm_probs = {}
+        self.tensor = []
+    def make_probs(self):
+        samples = numpy.random.normal(1,5,self.Num)
+        for i, prob in enumerate(samples):
+            self.arm_probs[i] = abs(prob)
+    def sample(self):
+        for i in range(self.Num):
+
+            X=Arm.gen(self.arm_probs[i],.00001,self.Num)
+            self.tensor.append(X)
+        self.tensor = numpy.array(self.tensor)
+        return self.tensor
+
+
+B = Arms(N)
+B.make_probs()
+out = B.sample()
+plt.imshow(out,cmap='hot')
+plt.show()
